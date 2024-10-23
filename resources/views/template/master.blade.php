57ea3afdfe67f95f94f9aed54b31d82e77e670ee
Allan <allanviannap@gmail.com>
2024-10-23 20:01:19 +0900

    Footer fixed and new texts adds

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -29,8 +29,6 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
-                        <li class="nav-item"><a class="nav-link me-lg-3" href="#features">Features</a></li>
-                        <li class="nav-item"><a class="nav-link me-lg-3" href="#download">Download</a></li>
                         @auth
                         <i class="bi bi-person-fill mt-2"></i>
                         <li class="nav-item">
@@ -49,48 +47,19 @@
         </nav>
 
         @yield('content')
-
-
-        <!-- Footer-->
-        <footer class="bg-black text-center py-5">
-            <div class="container px-5">
-                <div class="text-white-50 small">
-                    <div class="mb-2">CHANGE</div>
-                </div>
-            </div>
-        </footer>
-        <!-- Feedback Modal-->
-        <div class="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header bg-gradient-primary-to-secondary p-4">
-                        <h4 class="modal-title font-alt text-white" id="feedbackModalLabel">My account</h4>
-                        <button class="btn-close btn-close-white" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body border-0 p-4">
-                            <font size="5">
-                                <div class="form-floating mb-3">
-                                    <a class="nav-link rounded-pill px-3 mb-2 mb-lg-0" href="{{route('login')}}">
-                                        <i class="bi bi-dice-5"></i> &nbsp; My games</a>
-                                </div>
-                                
-                                <div class="form-floating mb-3">
-                                    <a class="nav-link rounded-pill px-3 mb-2 mb-lg-0" href="{{route('settings')}}">
-                                        <i class="bi bi-gear-fill"></i> &nbsp; Settings</a>
-                                </div>
-
-                                <div class="form-floating mb-3">
-                                    <a class="nav-link rounded-pill px-3 mb-2 mb-lg-0" href="{{route('logout')}}">
-                                        <i class="bi bi-door-closed"></i> &nbsp; Logout</a>
-                                </div>
-                            </font>
-                    </div>
-                </div>
-            </div>
-        </div>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
     </body>
+
+      <!-- Footer-->
+    <footer class=" footer bg-black text-center py-5">
+        <div class="container px-5">
+            <div class="text-white-50 small">
+                <div class="mb-2">CHANGE</div>
+            </div>
+        </div>
+    </footer>
+    
 </html>
